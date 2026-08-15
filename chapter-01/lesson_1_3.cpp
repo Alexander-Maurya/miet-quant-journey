b8371290c5fe9d5af6ad511995dde8f6cc50d19d
Alexander-Maurya <alexanderbmaurya@gmail.com>
2026-08-15 21:15:51 +0530

    Add lesson 1.3 variables and comments

---
+++ chapter-01/lesson_1_3.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // for std::cout to work
+
+int main()
+ {
+int x; // this creates variable with identifier x
+
+x=5 ; // this assigns value to variable
+
+// Printing the value assigned to variable
+std::cout << "The value of x is: " <<x ;
+
+return 0;
+}
